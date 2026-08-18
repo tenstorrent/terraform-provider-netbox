d95fb09291c591c912b93ae4cec0c09602193e7d
ndesousa-tt <ndesousa@tenstorrent.com>
2026-08-18 13:17:42 -0400

    docs: regenerate interface.md from template to pass docs check Add templates/resources/interface.md.tmpl and update the example file to remove direct mac_address usage. Run make docs to regenerate.

--- examples/resources/netbox_interface/resource.tf
+++ examples/resources/netbox_interface/resource.tf
@@ -12,10 +12,16 @@ resource "netbox_interface" "myvm_eth0" {
 resource "netbox_interface" "myvm_eth1" {
   name               = "eth1"
   enabled            = true
-  mac_address        = "00:16:3E:A8:B5:D7"
   mode               = "tagged"
   mtu                = 1440
   tagged_vlans       = [netbox_vlan.test1.id]
   untagged_vlan      = netbox_vlan.test2.id
   virtual_machine_id = netbox_virtual_machine.test.id
+}
+
+// To assign a MAC address, use netbox_mac_address with primary = true
+resource "netbox_mac_address" "myvm_eth0_mac" {
+  mac_address                  = "00:16:3E:A8:B5:D7"
+  virtual_machine_interface_id = netbox_interface.myvm_eth0.id
+  primary                      = true
 }
